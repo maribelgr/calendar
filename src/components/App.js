dee3edd85b315f0621570bd831804e4734d29e18
Maria Isabel Gragera Ruiz <mgrageraruiz@gmail.com>
2021-01-26 15:34:07 +0100

    Configure schedule library

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
+import React from 'react';
 import '../stylesheets/App.css';
+import { Inject, ScheduleComponent, Day, Week, WorkWeek, Month, Agenda } from '@syncfusion/ej2-react-schedule';
 
-function App() {
-  return (
-    <div className="App">
-    <p>Hola Mundo</p>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return <ScheduleComponent>
+      <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
+    </ScheduleComponent>
+  }
 }
 
 export default App;
